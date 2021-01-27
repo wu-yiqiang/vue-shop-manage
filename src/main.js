a56d8e3d86858c1b434a3f359fb61c3849e02311
YiQiangWu <wu_yiqiang@outlook.com>
2021-01-27 16:24:29 +0800

    天为输入框添加字体图标

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+//导入element的样式
 import './plugins/element.js'
+
+//导入iconfont字体图标
+import './assets/fonts/iconfont.css'
 
 // 导入全局样式
 import './assets/css/global.css'
